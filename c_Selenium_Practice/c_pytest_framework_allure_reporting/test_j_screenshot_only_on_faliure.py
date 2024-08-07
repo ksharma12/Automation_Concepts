a939c1f2d92e613706cc9867920ec9163db5a88b
ksharma12 <keshavsharma.tu@gmail.com>
2024-08-07 13:35:59 +0530

    New changes

--- c_Selenium_Practice/c_pytest_framework_allure_reporting/test_j_screenshot_only_on_faliure.py
+++ c_Selenium_Practice/c_pytest_framework_allure_reporting/test_j_screenshot_only_on_faliure.py
@@ -1,5 +1,4 @@
 # Pytest is unit testing framework for python users
-
 import allure
 import pytest
 from allure_commons.types import AttachmentType
@@ -12,7 +11,8 @@ def log_on_failure(request):
     yield
     item = request.node
     if item.rep_call.failed:
-        allure.attach(driver.get_screenshot_as_png(), name="login", attachment_type=AttachmentType.PNG)
+        allure.pytest_plugin.Attach(driver.get_screenshot_as_png(), name="login", attachment_type=AttachmentType.PNG)
+        # allure.attach(driver.get_screenshot_as_png(), name="login", attachment_type=AttachmentType.PNG)
 
 
 # run before each testcase in this file
@@ -36,6 +36,7 @@ def get_data():
         ("python@way2automation.com", "value_3")
     ]
 
+
 # testcase in this file
 @pytest.mark.usefixtures("log_on_failure")
 @pytest.mark.smoke
@@ -43,10 +44,10 @@ def test_loans_section():  # function continue execution even an assertion is fa
     assert "asdf" == "qwer"
     assert "zxcv" == "zxcv"
 
+
 # testcase in this file , this executes for all dataset present in above function
 @pytest.mark.usefixtures("log_on_failure")
 @pytest.mark.parametrize("username,password", get_data())
 def test_login_form(username, password):
     driver.find_element(By.ID, "email").send_keys(username)
     driver.find_element(By.ID, "pass").send_keys(password)
-
